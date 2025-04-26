840f38224c6e1fd2bd0ff1cd2a31e7bb5a77b050
dhimanDev06 <dhiman.mailme06@gmail.com>
2025-04-26 17:47:00 +0530

    banner dynamic

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,47 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import ApiService from "./service/apiService";
 import Carousel from 'react-bootstrap/Carousel';
-import img1 from "./assets/banner_1.jpg"
-import img2 from "./assets/banner_2.jpg"
-import img3 from "./assets/banner_3.jpg"
 
 function Banner() {
+  const [bannerList, setBannerList] = useState([]);
+
+  useEffect(() => {
+    ApiService.getBanner().then((res) => {
+      if (res.status === 200) {
+        setBannerList(res.data.data);
+      }
+    }).catch((er) => {
+      console.log(er);
+    });
+  }, []);
+
   return (
     <div className="container mt-4">
-    <Carousel className="w-100">
-      <Carousel.Item>
-        <img
-          className="d-block w-100 img-fluid"
-          src={img1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3 className='banner-title'>Water feature- Lotus pond</h3>
-          <p className='banner-desc'>A water feature can add beauty, tranquility, and a sense of serenity to any garden or outdoor space. It’s beautifully designed water feature with a floating white lotus-like structure made of marble.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 img-fluid"
-          src={img2}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3 className='banner-title'>Feature wall</h3>
-          <p  className='banner-desc'>A feature wall for a garden can be a beautiful focal point that enhances the overall look of your outdoor space.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 img-fluid"
-          src={img3}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3 className='banner-title'>Pavillion garden area</h3>
-          <p  className='banner-desc'>A pavilion in a garden area is a wonderful way to create a serene and functional outdoor space, ideal for relaxing.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+      <Carousel className="w-100">
+        {bannerList.map((banner) => (
+          <Carousel.Item key={banner.id}>
+            <img
+              className="d-block w-100 img-fluid"
+              src={'https://everestgreenscapegroup.com/backend/' + banner.image_path}
+              alt={banner.short_desc || "Banner image"}
+            />
+            <Carousel.Caption>
+              <h3 className="banner-title">{banner.short_desc}</h3>
+              <p className="banner-desc">{banner.description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 }
---
+++ src/service/apiService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+// Create an Axios instance with base URL
+
+// https://everestgreenscapegroup.com/backend/api.php?type=banner&status=active
+const egsClient = axios.create({
+  baseURL: "https://everestgreenscapegroup.com/backend", // Replace with your API URL
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// API service functions
+const ApiService = {
+  getBanner: () => egsClient.get("api.php?type=banner&status=active")
+};
+
+export default ApiService;
